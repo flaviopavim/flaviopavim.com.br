7b55e76d2144446d416b48a2d3836800c6ccd47b
Flávio Pavim <contato@flaviopavim.com.br>
2024-05-26 21:01:11 -0300

    More Javascript and CSS

--- js/adjust.js
+++ js/adjust.js
@@ -1,6 +1,7 @@
 function adjust() {
     $('.page').css({maxHeight:$(window).height()-150});
     if (!opened) {
+//        $('#home-button,#works-button,#skills-button,#videos-button,#contact-button').css({width: '20%'});
         $('.nav ul li').css({width: '20%'});
     }
 }
